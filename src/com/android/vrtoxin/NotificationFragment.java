c5d7ce36ca5d915430b08e605d413154c0415b23
NL-BlackDragon <blackdragon.vrtoxin@gmail.com>
2016-01-22 17:54:37 +0100

    Media & Notifications: Add some more options
    Added:
    * Safe headset volume
    * Screenshot sound
    * Less notification sounds
    * Volume dialog color
    * Power notification sounds
    
    Signed-off-by: NL-BlackDragon <blackdragon.vrtoxin@gmail.com>

--- src/com/android/vrtoxin/NotificationFragment.java
+++ src/com/android/vrtoxin/NotificationFragment.java
@@ -16,20 +16,34 @@
 
 package com.android.vrtoxin;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.ComponentName;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.hardware.fingerprint.FingerprintManager;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.SystemProperties;
 import android.os.UserHandle;
+import android.os.Vibrator;
 import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.Preference;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
+import android.preference.ListPreference;
 import android.preference.PreferenceScreen;
 import android.preference.SwitchPreference;
-import android.support.annotation.NonNull;
+import android.provider.Settings;
+import android.provider.Settings.Global;
+import android.provider.Settings.System;
+import android.util.Log;
 
 import com.android.vrtoxin.preferences.SystemSettingSwitchPreference;
 
@@ -37,6 +51,8 @@ public class NotificationFragment extends PreferenceFragment implements
          OnPreferenceChangeListener {
 
     public NotificationFragment(){}
+
+    private static final String TAG = "NotificationFragment";
 
     private FingerprintManager mFingerprintManager;
     private SystemSettingSwitchPreference mFingerprintVib;
@@ -47,6 +63,8 @@ public class NotificationFragment extends PreferenceFragment implements
     public static final String KEY_NOTIF_MANAGER_CLASS_NAME = "com.android.settings.Settings$NotificationManagerSettingsActivity";
     public static final String KEY_NOTIF_HEADS_UP_CLASS_NAME = "com.android.settings.Settings$HeadsUpSettingsActivity";
 
+    private static final int DLG_SAFE_HEADSET_VOLUME = 0;
+
     private static final String NOTIF_BREATHING = "breathing_notifications";
     private static final String NOTIF_COLOR = "notification_colors";
     private static final String KEY_NOTIFICATION = "notification";
@@ -56,6 +74,19 @@ public class NotificationFragment extends PreferenceFragment implements
     private static final String KEY_CAMERA_SOUNDS = "camera_sounds";
     private static final String PROP_CAMERA_SOUND = "persist.sys.camera-sound";
     private static final String NOTIF_HEADS_UP = "heads_up_notif";
+    private static final String KEY_SAFE_HEADSET_VOLUME = "safe_headset_volume";
+    private static final String PREF_LESS_NOTIFICATION_SOUNDS = "less_notification_sounds";
+    private static final String KEY_VOLUME_DIALOG_COLOR = "volume_dialog_frag";
+
+    private static final String KEY_POWER_NOTIFICATIONS = "power_notifications";
+    private static final String KEY_POWER_NOTIFICATIONS_VIBRATE = "power_notifications_vibrate";
+    private static final String KEY_POWER_NOTIFICATIONS_RINGTONE = "power_notifications_ringtone";
+
+    // Request code for power notification ringtone picker
+    private static final int REQUEST_CODE_POWER_NOTIFICATIONS_RINGTONE = 1;
+
+    // Used for power notification uri string if set to silent
+    private static final String POWER_NOTIFICATIONS_SILENT_URI = "silent";
 
     private Preference mBreathing;
     private Preference mColor;
@@ -64,6 +95,14 @@ public class NotificationFragment extends PreferenceFragment implements
     private Preference mNotifManager;
     private SwitchPreference mCameraSounds;
     private Preference mHeadsUp;
+    private SwitchPreference mSafeHeadsetVolume;
+    private ListPreference mAnnoyingNotifications;
+    private Preference mVolumeDialogColor;
+    private SwitchPreference mPowerSounds;
+    private SwitchPreference mPowerSoundsVibrate;
+    private Preference mPowerSoundsRingtone;
+
+    private ContentResolver mResolver;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -71,6 +110,7 @@ public class NotificationFragment extends PreferenceFragment implements
 
         addPreferencesFromResource(R.xml.notification_fragment);
         final PreferenceScreen prefScreen = getPreferenceScreen();
+        mResolver = getActivity().getContentResolver();
 
         final PreferenceCategory notification = (PreferenceCategory)
                 findPreference(KEY_NOTIFICATION);
@@ -82,10 +122,57 @@ public class NotificationFragment extends PreferenceFragment implements
         mNotifLight = (Preference)findPreference(NOTIF_LIGHT);
         mNotifManager = (Preference)findPreference(NOTIF_MANAGER);
         mHeadsUp = (Preference)findPreference(NOTIF_HEADS_UP);
+        mVolumeDialogColor = (Preference)findPreference(KEY_VOLUME_DIALOG_COLOR);
 
         mCameraSounds = (SwitchPreference) findPreference(KEY_CAMERA_SOUNDS);
         mCameraSounds.setChecked(SystemProperties.getBoolean(PROP_CAMERA_SOUND, true));
         mCameraSounds.setOnPreferenceChangeListener(this);
+
+        mSafeHeadsetVolume = (SwitchPreference) findPreference(KEY_SAFE_HEADSET_VOLUME);
+        mSafeHeadsetVolume.setChecked(Settings.System.getInt(mResolver,
+                Settings.System.SAFE_HEADSET_VOLUME, 1) != 0);
+        mSafeHeadsetVolume.setOnPreferenceChangeListener(this);
+
+        mAnnoyingNotifications = (ListPreference) findPreference(PREF_LESS_NOTIFICATION_SOUNDS);
+        int notificationThreshold = Settings.System.getInt(mResolver,
+                Settings.System.MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD,
+                0);
+        mAnnoyingNotifications.setValue(Integer.toString(notificationThreshold));
+        mAnnoyingNotifications.setOnPreferenceChangeListener(this);
+
+        // power state change notification sounds
+        mPowerSounds = (SwitchPreference) findPreference(KEY_POWER_NOTIFICATIONS);
+        mPowerSounds.setChecked(Global.getInt(mResolver,
+                Global.POWER_NOTIFICATIONS_ENABLED, 0) != 0);
+        mPowerSoundsVibrate = (SwitchPreference) findPreference("power_notifications_vibrate");
+        mPowerSoundsVibrate.setChecked(Global.getInt(mResolver,
+                Global.POWER_NOTIFICATIONS_VIBRATE, 0) != 0);
+        Vibrator vibrator = (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
+        if (vibrator == null || !vibrator.hasVibrator()) {
+            prefScreen.removePreference(mPowerSoundsVibrate);
+        }
+
+        mPowerSoundsRingtone = findPreference(KEY_POWER_NOTIFICATIONS_RINGTONE);
+        String currentPowerRingtonePath =
+                Global.getString(mResolver, Global.POWER_NOTIFICATIONS_RINGTONE);
+
+        // set to default notification if we don't yet have one
+        if (currentPowerRingtonePath == null) {
+                currentPowerRingtonePath = System.DEFAULT_NOTIFICATION_URI.toString();
+                Global.putString(mResolver,
+                        Global.POWER_NOTIFICATIONS_RINGTONE, currentPowerRingtonePath);
+        }
+        // is it silent ?
+        if (currentPowerRingtonePath.equals(POWER_NOTIFICATIONS_SILENT_URI)) {
+            mPowerSoundsRingtone.setSummary(
+                    getString(R.string.power_notifications_ringtone_silent));
+        } else {
+            final Ringtone ringtone =
+                    RingtoneManager.getRingtone(getActivity(), Uri.parse(currentPowerRingtonePath));
+            if (ringtone != null) {
+                mPowerSoundsRingtone.setSummary(ringtone.getTitle(getActivity()));
+            }
+        }
 
         mFingerprintManager = (FingerprintManager) getActivity().getSystemService(Context.FINGERPRINT_SERVICE);
         mFingerprintVib = (SystemSettingSwitchPreference) prefScreen.findPreference("fingerprint_success_vib");
@@ -107,20 +194,20 @@ public class NotificationFragment extends PreferenceFragment implements
     }
 
     @Override
-    public boolean onPreferenceTreeClick(PreferenceScreen prefScreen, @NonNull Preference pref) {
-        if (pref == mBreathing) {
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mBreathing) {
             ((VRToxinActivity)getActivity()).displaySubFrag(getString(R.string.breathing_notifications_frag_title));
 
             return true;
         }
 
-        if (pref == mColor) {
+        if (preference == mColor) {
             ((VRToxinActivity)getActivity()).displaySubFrag(getString(R.string.notification_color_frag_title));
 
             return true;
         }
 
-        if (pref == mBatteryLight) {
+        if (preference == mBatteryLight) {
             Intent action = new Intent(Intent.ACTION_MAIN);
             ComponentName cn = new ComponentName(KEY_NOTIFICATION_ACTIVITY_PACKAGE_NAME, KEY_NOTIF_BATTERY_LIGHT_CLASS_NAME);
             action.setComponent(cn);
@@ -129,7 +216,7 @@ public class NotificationFragment extends PreferenceFragment implements
             return true;
         }
 
-        if (pref == mNotifLight) {
+        if (preference == mNotifLight) {
             Intent action = new Intent(Intent.ACTION_MAIN);
             ComponentName cn = new ComponentName(KEY_NOTIFICATION_ACTIVITY_PACKAGE_NAME, KEY_NOTIF_LIGHT_CLASS_NAME);
             action.setComponent(cn);
@@ -138,7 +225,7 @@ public class NotificationFragment extends PreferenceFragment implements
             return true;
         }
 
-        if (pref == mNotifManager) {
+        if (preference == mNotifManager) {
             Intent action = new Intent(Intent.ACTION_MAIN);
             ComponentName cn = new ComponentName(KEY_NOTIFICATION_ACTIVITY_PACKAGE_NAME, KEY_NOTIF_MANAGER_CLASS_NAME);
             action.setComponent(cn);
@@ -147,7 +234,7 @@ public class NotificationFragment extends PreferenceFragment implements
             return true;
         }
 
-        if (pref == mHeadsUp) {
+        if (preference == mHeadsUp) {
             Intent action = new Intent(Intent.ACTION_MAIN);
             ComponentName cn = new ComponentName(KEY_NOTIFICATION_ACTIVITY_PACKAGE_NAME, KEY_NOTIF_HEADS_UP_CLASS_NAME);
             action.setComponent(cn);
@@ -156,7 +243,32 @@ public class NotificationFragment extends PreferenceFragment implements
             return true;
         }
 
-        return false;
+        if (preference == mVolumeDialogColor) {
+            ((VRToxinActivity)getActivity()).displaySubFrag(getString(R.string.volume_dialog_frag_title));
+
+            return true;
+        }
+
+        if (preference == mPowerSounds) {
+            Global.putInt(mResolver,
+                    Global.POWER_NOTIFICATIONS_ENABLED,
+                    mPowerSounds.isChecked() ? 1 : 0);
+
+        } else if (preference == mPowerSoundsVibrate) {
+            Global.putInt(mResolver,
+                    Global.POWER_NOTIFICATIONS_VIBRATE,
+                    mPowerSoundsVibrate.isChecked() ? 1 : 0);
+
+        } else if (preference == mPowerSoundsRingtone) {
+            launchNotificationSoundPicker(REQUEST_CODE_POWER_NOTIFICATIONS_RINGTONE,
+                    Global.getString(mResolver,
+                            Global.POWER_NOTIFICATIONS_RINGTONE));
+        } else {
+            // If we didn't handle it, let preferences handle it.
+            return super.onPreferenceTreeClick(preferenceScreen, preference);
+        }
+
+        return true;
     }
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
@@ -168,6 +280,131 @@ public class NotificationFragment extends PreferenceFragment implements
                SystemProperties.set(PROP_CAMERA_SOUND, "0");
            }
         }
+        if (KEY_SAFE_HEADSET_VOLUME.equals(key)) {
+            if ((Boolean) objValue) {
+                Settings.System.putInt(mResolver,
+                        Settings.System.SAFE_HEADSET_VOLUME, 1);
+            } else {
+                showDialogInner(DLG_SAFE_HEADSET_VOLUME);
+            }
+        }
+        if (PREF_LESS_NOTIFICATION_SOUNDS.equals(key)) {
+            final int val = Integer.valueOf((String) objValue);
+            Settings.System.putInt(mResolver,
+                    Settings.System.MUTE_ANNOYING_NOTIFICATIONS_THRESHOLD, val);
+        }
         return true;
+    }
+
+    private void showDialogInner(int id) {
+        DialogFragment newFragment = MyAlertDialogFragment.newInstance(id);
+        newFragment.setTargetFragment(this, 0);
+        newFragment.show(getFragmentManager(), "dialog " + id);
+    }
+
+    public static class MyAlertDialogFragment extends DialogFragment {
+
+        public static MyAlertDialogFragment newInstance(int id) {
+            MyAlertDialogFragment frag = new MyAlertDialogFragment();
+            Bundle args = new Bundle();
+            args.putInt("id", id);
+            frag.setArguments(args);
+            return frag;
+        }
+
+        NotificationFragment getOwner() {
+            return (NotificationFragment) getTargetFragment();
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            int id = getArguments().getInt("id");
+            switch (id) {
+                case DLG_SAFE_HEADSET_VOLUME:
+                    return new AlertDialog.Builder(getActivity())
+                    .setTitle(R.string.attention)
+                    .setMessage(R.string.safe_headset_volume_warning_dialog_text)
+                    .setPositiveButton(R.string.dlg_ok,
+                        new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            Settings.System.putInt(getOwner().mResolver,
+                                    Settings.System.SAFE_HEADSET_VOLUME, 0);
+
+                        }
+                    })
+                    .setNegativeButton(R.string.cancel,
+                        new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    })
+                    .create();
+            }
+            throw new IllegalArgumentException("unknown id " + id);
+        }
+
+        @Override
+        public void onCancel(DialogInterface dialog) {
+            int id = getArguments().getInt("id");
+            switch (id) {
+                case DLG_SAFE_HEADSET_VOLUME:
+                    getOwner().mSafeHeadsetVolume.setChecked(true);
+                    break;
+            }
+        }
+    }
+
+    private void launchNotificationSoundPicker(int code, String currentPowerRingtonePath) {
+        final Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
+
+        intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TITLE,
+                getString(R.string.power_notifications_ringtone_title));
+        intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TYPE,
+                RingtoneManager.TYPE_NOTIFICATION);
+        intent.putExtra(RingtoneManager.EXTRA_RINGTONE_DEFAULT_URI,
+                System.DEFAULT_NOTIFICATION_URI);
+        if (currentPowerRingtonePath != null &&
+                !currentPowerRingtonePath.equals(POWER_NOTIFICATIONS_SILENT_URI)) {
+            Uri uri = Uri.parse(currentPowerRingtonePath);
+            if (uri != null) {
+                intent.putExtra(RingtoneManager.EXTRA_RINGTONE_EXISTING_URI, uri);
+            }
+        }
+        startActivityForResult(intent, code);
+    }
+
+    private void setPowerNotificationRingtone(Intent intent) {
+        final Uri uri = intent.getParcelableExtra(RingtoneManager.EXTRA_RINGTONE_PICKED_URI);
+
+        final String toneName;
+        final String toneUriPath;
+
+        if ( uri != null ) {
+            final Ringtone ringtone = RingtoneManager.getRingtone(getActivity(), uri);
+            toneName = ringtone.getTitle(getActivity());
+            toneUriPath = uri.toString();
+        } else {
+            // silent
+            toneName = getString(R.string.power_notifications_ringtone_silent);
+            toneUriPath = POWER_NOTIFICATIONS_SILENT_URI;
+        }
+
+        mPowerSoundsRingtone.setSummary(toneName);
+        Global.putString(mResolver,
+                Global.POWER_NOTIFICATIONS_RINGTONE, toneUriPath);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_CODE_POWER_NOTIFICATIONS_RINGTONE:
+                if (resultCode == Activity.RESULT_OK) {
+                    setPowerNotificationRingtone(data);
+                }
+                break;
+            default:
+                super.onActivityResult(requestCode, resultCode, data);
+                break;
+        }
     }
 }
